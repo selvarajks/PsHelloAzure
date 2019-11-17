9c54e6a1eba1c2a90d28ec840ed48e3175132390
selvarajks <selvarajks@gmail.com>
2019-11-17 11:49:34 +0800

    updating ef changes

--- PsHelloAzure/Program.cs
+++ PsHelloAzure/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PsHelloAzure.Data;
 
 namespace PsHelloAzure
 {
@@ -14,11 +17,28 @@ namespace PsHelloAzure
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            
+            //CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+            MigrateDatabase(host);
+            host.Run();
+
+
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        public static void MigrateDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
+        }
     }
 }
